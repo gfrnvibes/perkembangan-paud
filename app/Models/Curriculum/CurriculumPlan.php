bf8e53d39b67f3ce6a37333f0918cfb527364d75
Gufron <gupron.nurjalil14@gmail.com>
2025-12-24 07:04:49 +0700

    curricullum

--- app/Models/Curriculum/CurriculumPlan.php
+++ app/Models/Curriculum/CurriculumPlan.php
@@ -2,9 +2,34 @@
 
 namespace App\Models\Curriculum;
 
+use App\Models\Master\AcademicYear;
+use App\Models\Master\CpElement;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CurriculumPlan extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['academic_year_id', 'semester', 'week_number', 'theme'];
+
+    public function academicYear()
+    {
+        return $this->belongsTo(AcademicYear::class);
+    }
+
+    public function cpElements()
+    {
+        return $this->belongsToMany(CpElement::class, 'plan_cp_element');
+    }
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+
+    public function learningObjectives()
+    {
+        return $this->hasMany(LearningObjective::class);
+    }
 }
--- app/Models/Curriculum/LearningObjective.php
+++ app/Models/Curriculum/LearningObjective.php
@@ -3,8 +3,12 @@
 namespace App\Models\Curriculum;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LearningObjective extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = ['curriculum_plan_id', 'description'];
+
+    public function curriculumPlan() { return $this->belongsTo(CurriculumPlan::class); }
 }
